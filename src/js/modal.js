d3be140d3c4c0290be32d9c79d27d81fc6c46241
Marian-Shperun <marik2504911@gmail.com>
2022-06-01 19:35:35 +0200

    правки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,30 @@
 (() => {
   const refs = {
-    openModalBtn1: document.querySelector("[data-modal-open-1]"),
-    openModalBtn2: document.querySelector("[data-modal-open-2]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    openModalBtn1: document.querySelector('[data-modal-open-1]'),
+    openModalBtn2: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
 
-  refs.openModalBtn1.addEventListener("click", toggleModal);
-  refs.openModalBtn2.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    openModalContact: document.querySelectorAll('[contacts-btn]'),
+    closeModalContact: document.querySelector('[modal-contacts-close]'),
+    modalContacts: document.querySelector('[modal-contacts]'),
+  };
+  refs.openModalContact.forEach(el => {
+    el.addEventListener('click', () => {
+      refs.modalContacts.classList.toggle('is-hidden');
+    });
+  });
+
+  refs.closeModalContact.addEventListener('click', () => {
+    refs.modalContacts.classList.toggle('is-hidden');
+  });
+
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
+
