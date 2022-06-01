4e9f4135b0d3f11505a294f434a0197eb5259618
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-06-01 13:22:08 +0300

    fix about, add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn1: document.querySelector("[data-modal-open-1]"),
-    openModalBtn2: document.querySelector("[data-modal-open-2]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn1: document.querySelector('[data-modal-open-1]'),
+    openModalBtn2: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn1.addEventListener("click", toggleModal);
-  refs.openModalBtn2.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
